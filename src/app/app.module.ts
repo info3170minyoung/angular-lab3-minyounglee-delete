3daf355fac4d55054f241739fcaf814209feb94b
info3170minyoung <113271790+info3170minyoung@users.noreply.github.com>
2022-11-04 06:58:31 -0400

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,19 @@ import { ProductsListComponent } from './products-list/products-list.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AboutUsComponent } from './about-us/about-us.component';
+import { HowtocareComponent } from './howtocare/howtocare.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     RouterModule.forRoot([
+      { path: 'aboutus', component: AboutUsComponent },
       { path: 'products', component: ProductsListComponent },
       { path: 'cart', component: ShoppingCartComponent },
-      { path: '', redirectTo: '/products', pathMatch: 'full' },
-      { path: '**', component: PageNotFoundComponent },
+      { path: 'howtocare', component: HowtocareComponent },
+      { path: '', redirectTo: '/howtocare', pathMatch: 'full' },
+      { path: 'notfound', component: PageNotFoundComponent },
+      { path: '**', redirectTo: '/notfound', pathMatch: 'full' },
     ]),
     FormsModule,
   ],
@@ -26,6 +30,7 @@ import { AboutUsComponent } from './about-us/about-us.component';
     ShoppingCartComponent,
     PageNotFoundComponent,
     AboutUsComponent,
+    HowtocareComponent,
   ],
   bootstrap: [AppComponent],
 })
---
+++ src/app/howtocare/howtocare.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-howtocare',
+  templateUrl: './howtocare.component.html',
+  styleUrls: ['./howtocare.component.css']
+})
+export class HowtocareComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
